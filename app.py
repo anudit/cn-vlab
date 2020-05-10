477d2ac6cc489d71661bceeca958c19885ddba6e
Anudit Nagar <nagaranudit@gmail.com>
2020-05-10 17:51:46 +0530

    Cleanup Integration

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route('/image/<bin>')
 def image(bin):
-    url = f"http://www.ee.unb.ca/cgi-bin/tervo/encoding.pl?binary={bin}&c=1&d=1"
-    response = requests.request("GET", url)
-    html = response.text.encode('utf8')
-    soup = BeautifulSoup(html, features='html5lib')
-    img = soup.find_all('img')[1].attrs['src']
-    imgLink = f'http://www.ee.unb.ca/cgi-bin/tervo/{img}'
-    return imgLink
+   url = f"http://www.ee.unb.ca/cgi-bin/tervo/encoding.pl?binary={bin}&c=1&d=1"
+   response = requests.request("GET", url)
+   html = response.text.encode('utf8')
+   soup = BeautifulSoup(html, features='html5lib')
+   img = soup.find_all('img')[1].attrs['src']
+   imgLink = f'http://www.ee.unb.ca/cgi-bin/tervo/{img}'
+   jsonData = jsonify({
+      "url":imgLink
+   })
+   return jsonData
 
 
 @app.route('/')
