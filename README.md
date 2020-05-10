87bc21a61b633b7fc6521288fad8d1241c6ee71d
Anudit Nagar <nagaranudit@gmail.com>
2020-05-10 15:50:37 +0530

    Add Basic UI

---
+++ README.md
@@ -0,0 +1,8 @@
+## Computer Network Encoding - VLAB
+
+### runnning the project
+
+```
+pip install -r requirements.txt
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/image/<bin>')
+def image(bin):
+    url = f"http://www.ee.unb.ca/cgi-bin/tervo/encoding.pl?binary={bin}&c=1&d=1"
+    response = requests.request("GET", url)
+    html = response.text.encode('utf8')
+    soup = BeautifulSoup(html, features='html5lib')
+    img = soup.find_all('img')[1].attrs['src']
+    imgLink = f'http://www.ee.unb.ca/cgi-bin/tervo/{img}'
+    return imgLink
+
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(debug=True)
