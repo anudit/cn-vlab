d6b06fc28bf5b5c8281beaf91d049a6be2226025
Anudit Nagar <nagaranudit@gmail.com>
2020-05-27 21:02:04 +0530

    Quick Fix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def image(bin):
    url = f"http://www.ee.unb.ca/cgi-bin/tervo/encoding.pl?binary={bin}&c=1&d=1"
    response = requests.request("GET", url)
    html = response.text.encode('utf8')
-   soup = BeautifulSoup(html, features='html5lib')
+   soup = BeautifulSoup(html, 'html.parser')
    img = soup.find_all('img')[1].attrs['src']
    imgLink = f'http://www.ee.unb.ca/cgi-bin/tervo/{img}'
    jsonData = jsonify({
